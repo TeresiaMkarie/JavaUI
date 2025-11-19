8caea51b7d76301f68d09d9f21c34a8ddceda0f9
Teresia Mkarie Jomo <teresiajomo1@gmail.com>
2025-11-19 11:59:19 +0300

    Fix bounds for pesa button in Loans.java

--- Loans.java
+++ Loans.java
@@ -1,3 +1,4 @@
+
 import javax.swing.*;
 import javax.swing.border.Border;
 
@@ -155,7 +156,7 @@ Border  border = BorderFactory.createLineBorder(Color.WHITE,1);
     
    });
 
-   pesa.setBounds(20, 350, 80, 30);
+   pesa.setBounds(60, 300git, 80, 30);
    pesa.setBackground(new Color(0x1f87fc));
    pesa.setForeground(new Color(0xffffff));
    pesa.setFont(new Font("MV BOLI", Font.BOLD, 18));
